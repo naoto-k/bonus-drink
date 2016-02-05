902c2ff28cc1786a8d1239c5653c878856e458d4
Naoto Kashita <naoto.kashita@livesense.co.jp>
2016-02-05 18:36:33 +0900

    Answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,11 @@
 class BonusDrink
+  AMOUNT_NECESARY_FOR_BONUS = 3
   def self.total_count_for(amount)
-    0
+    empty = amount
+    while empty >= AMOUNT_NECESARY_FOR_BONUS do
+      amount += empty / AMOUNT_NECESARY_FOR_BONUS
+      empty = empty % AMOUNT_NECESARY_FOR_BONUS + empty / AMOUNT_NECESARY_FOR_BONUS
+    end
+    amount
   end
 end
